0a162295781ae899f0d7a75f3698867f0507fc9b
Joseph Sly <156633695+JES-00@users.noreply.github.com>
2024-11-24 14:39:37 -0600

    Create script.js

---
+++ 25 + 5 Clock/script.js
@@ -0,0 +1,139 @@
+import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+import * as React from "https://cdn.skypack.dev/react@17.0.1";
+
+const App = () => {
+  const [breakLength, setBreakLength] = React.useState(5);
+  const [sessionLength, setSessionLength] = React.useState(25);
+  const [timeLeft, setTimeLeft] = React.useState(1500);
+  const [timingType, setTimingtype] = React.useState("SESSION");
+  
+  const [play, setPlay] = React.useState(false);
+  
+  const timeout = setTimeout(() => {
+    if(timeLeft && play){
+      setTimeLeft(timeLeft - 1)
+    }
+  }, 1000);
+
+   const sessionIncrease = () => {
+    if(sessionLength < 60){
+      setSessionLength(sessionLength + 1)
+      setTimeLeft(timeLeft + 60)
+    }
+  }
+  
+  const sessionDecrease = () => {
+    if(sessionLength > 1){
+      setSessionLength(sessionLength - 1)
+      setTimeLeft(timeLeft - 60)
+    }
+  }
+  
+  const breakIncrease = () => {
+    if(breakLength < 60){
+      setBreakLength(breakLength + 1)
+    }
+  }
+  
+  const breakDecrease = () => {
+    if(breakLength > 1){
+      setBreakLength(breakLength - 1)
+    }
+  }
+  
+  const handleReset = () => {
+    clearTimeout(timeout);
+    setPlay(false);
+    setTimeLeft(1500);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimingtype("SESSION");
+    const audio = document.getElementById("beep");
+    audio.pause()
+    audio.currentTime = 0;
+  }
+  
+  const handlePlay = () => {
+    clearTimeout(timeout);
+    setPlay(!play);
+  }
+  
+  const resetTimer = () => {
+    const audio = document.getElementById("beep");
+    if(!timeLeft && timingType === "SESSION"){
+      setTimeLeft(breakLength * 60)
+      setTimingtype("BREAK")
+      audio.play()
+    }
+    if(!timeLeft && timingType === "BREAK"){
+      setTimeLeft(sessionLength * 60)
+      setTimingtype("SESSION")
+      audio.pause()
+      audio.currentTime = 0;
+    }
+  }
+  
+  const clock = () => {
+    if(play){
+      timeout
+      resetTimer()
+    }else {
+      clearTimeout(timeout)
+    }
+  }
+  
+  React.useEffect(() => {
+    clock()
+  }, [play, timeLeft, timeout])
+ 
+  const timeFormatter = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+  
+  const title = timingType === "SESSION" ? "Session" : "Break";
+
+  return (
+   <div>
+    <div className="wrapper">
+      <h2>25 + 5 Clock</h2>
+      <h4>Also known as the pomodoro technique, you work in a twenty-five minute long session, and you take a break for five minutes. Rinse and repeat.</h4>
+      <div className="break-session-length">
+                 <div>
+           <h3 id="session-label">Session Length</h3>
+           <div>
+            <button disabled={play} onClick={sessionIncrease} id="session-increment" title="Add 1 Minute to your Session">+</button>
+              <strong id="session-length">{sessionLength}</strong>
+            <button disabled={play} onClick={sessionDecrease} id="session-decrement" title="Reduce 1 Minute from your Session">-</button>
+          </div>
+         </div>
+        <div>
+          <h3 id="break-label">Break Length</h3>
+          <div>
+            <button disabled={play} onClick={breakIncrease} id="break-increment" title="Add 1 minute to your Break">+</button>
+              <strong id="break-length">{breakLength}</strong>
+            <button disabled={play} onClick={breakDecrease} id="break-decrement" title="Reduce 1 minute from your Break">-</button>
+          </div>
+         </div>
+      </div>
+      <div className="timer-wrapper">
+        <div className="timer">
+           <h2 id="timer-label">{title}</h2>
+           <h3 id="time-left">{timeFormatter()}</h3>
+        </div>
+        <button onClick={handlePlay} id="start_stop" title="Start a New Session or Pause It!">Start/Pause</button>
+        <button onClick={handleReset} id="reset" title="Reset Session">Reset</button>
+      </div>
+    </div>
+    <audio
+      id="beep" 
+      preload="auto"
+      src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+    />
+    </div>);
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
